7f25491579978b2df55395a7cf77e930e50364ac
demetres <demetres.kostas@gmail.com>
2017-02-17 20:24:00 -0500

    Cleanup: missed a file, and had IDE directory commited.

---
+++ MATLAB/correlationFigure.m
@@ -0,0 +1,42 @@
+function [ xlabels, ylabels ] = correlationFigure( A, meglabels, audiolabels, numticks, plot_title )
+%CORRELATIONFIGURE Generate a figure showing a visualiztion of the provided
+% matrix
+%
+% A  - matrix n x m, where m is dimension of meg features, and n the audio
+% meglabels - The labels for the meg features
+% audiolabels - The labels for the audio features
+% numticks - The number of highest value points to label on the axis
+
+if length(size(A)) ~= 2, fprintf('A must be 2 dimensions\n'); return; end
+B = abs(A);
+
+figure;
+get(gca);
+ax = gca;
+imagesc(B);
+colorbar
+
+title(plot_title);
+ax.XLabel.String = 'MEG Features';
+ax.YLabel.String = 'Acoustic Features';
+
+% Sort the array entries to show the highest activity
+B(isnan(B)) = -1;
+[~, ind] = sort(B(:), 'descend');
+
+% mark highest activity
+[y, x] = ind2sub(size(B), ind(1:numticks));
+
+x = unique(x);
+y = unique(y);
+    
+ax.XTick = x;
+ax.YTick = y;
+    
+ax.XTickLabel = meglabels(x);
+ax.YTickLabel = audiolabels(y);
+xlabels = meglabels(x);
+ylabels = audiolabels(y);
+
+end
+
