6dc33ae1265c06e2d0bd66458bce2524ff5527df
demetres <demetres.kostas@gmail.com>
2017-03-15 18:39:23 -0400

    ...

--- MATLAB/eeg_export.m
+++ MATLAB/eeg_export.m
@@ -5,7 +5,6 @@ function [ output_args ] = eeg_export( ALLEEG , destination, ind )
 [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, ALLEEG, 1, 'retrieve', ind, 'study', 1);
 EEG = eeg_checkset(EEG);
 fprintf('Loaded %s\nExporting...\n', EEG.setname);
-MEGEEG_CHANNELS = 37:187;
 
 %meg_outdir = strcat(EEG.filepath, '/MEG/', EEG.condition, '/');
 %audio_outdir = strcat(EEG.filepath, '/Audio/', EEG.condition, '/');
@@ -22,7 +21,7 @@ for j=1:size(EEG.icaact, 3)
     % transpose for opensmile
     f = fullfile(meg_outdir, strcat('/epoch_', int2str(j), '.csv'));
     if exist(fullfile(meg_outdir, strcat('/epoch_', int2str(j), '.bak'))) == 2, continue; end
-    csvwrite(f, EEG.icaact(MEGEEG_CHANNELS,:,j)');
+    csvwrite(f, EEG.icaact(:,:,j)');
     csvwrite(fullfile(audio_outdir, strcat('/epoch_', int2str(j), '.csv')), EEG.acoustic(:,j));
     fprintf('.');
 end
