a5e0304ca6f9ec1388abc7b3c6372b57a0e8abcb
demetres <demetres.kostas@gmail.com>
2017-03-15 18:36:19 -0400

    update to export script

--- MATLAB/eeg_export.m
+++ MATLAB/eeg_export.m
@@ -1,4 +1,4 @@
-function [ output_args ] = eeg_export( ALLEEG , ind )
+function [ output_args ] = eeg_export( ALLEEG , destination, ind )
 %EEG_EXPORT Summary of this function goes here
 %   Detailed explanation goes here
 
